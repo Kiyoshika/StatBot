f214c83fdb9657faa2bf9f2ff5467b863bbf1ef2
Kiyoshika <zachlweaver00@gmail.com>
2021-12-05 23:59:42 -0500

    Initial bot setup
    Bare minimum code to establish connection with Discord client. Reading bot token from bottoken.cfg

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+    public static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+    private DiscordSocketClient _client;
+    public async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
+        var token = File.ReadAllText("bottoken.cfg");
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+}
