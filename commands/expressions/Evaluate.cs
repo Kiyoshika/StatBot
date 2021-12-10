0f0818a75415de1a85ec3b5d14e02fe72eeca73a
Kiyoshika <zachlweaver00@gmail.com>
2021-12-09 19:43:13 -0500

    started wrapper for DataTable for flexibility
    created the skeleton for the wrapper around DataTable to handle more complicated expressions involving common mathematical functions

--- commands/expressions/Evaluate.cs
+++ commands/expressions/Evaluate.cs
@@ -12,7 +12,7 @@ public class Evaluate : ModuleBase<SocketCommandContext>
     [Command("evaluate")]
     public async Task evaluate(string expression)
     {
-        double value = ExpressionEvaluator.evaluateExpression(expression);
+        double value = ExpressionEvaluator.evaluateExpression(ref expression);
         Context.Channel.SendMessageAsync($"Result: {value}");
     }
 }
--- internal/expressions/ExpressionEvaluator.cs
+++ internal/expressions/ExpressionEvaluator.cs
@@ -3,10 +3,39 @@ using System.Data;
 
 public class ExpressionEvaluator
 {
-    public static dynamic evaluateExpression(string expression)
+    // this will return a tuple where the first element indicates if any
+    // special functions are found and the second element will be the list
+    // of all special functions contained. It will be empty if none are found.
+    private static Tuple<bool, List<string>> checkSpecialFunctions(ref string expression)
+    {
+        bool containsSpecialFunction = false;
+        List<string> specialFunctions = new List<string>();
+        if (expression.Contains("sqrt")) { containsSpecialFunction = true; specialFunctions.Add("sqrt"); }
+        if (expression.Contains("log")) { containsSpecialFunction = true; specialFunctions.Add("log"); }
+        if (expression.Contains("pow")) { containsSpecialFunction = true; specialFunctions.Add("pow"); }
+        if (expression.Contains("exp")) { containsSpecialFunction = true; specialFunctions.Add("exp"); }
+
+        Tuple<bool, List<string>> return_values = new Tuple<bool, List<string>>(containsSpecialFunction, specialFunctions);
+        return return_values;
+    }
+
+    // this works for simple expressions but doesn't support generic functions like
+    // sqrt, log, ^ (powers), etc.
+    public static dynamic evaluateExpression_legacy(ref string expression)
     {
         DataTable dt = new DataTable();
         var v = dt.Compute(expression,"");
         return v;
+    }
+
+    // this is a modified parser to handle the special functions before passing the
+    // final expression to DataTable
+    public static dynamic evaluateExpression(ref string expression)
+    {
+        // check if any special functions are contained
+        Tuple<bool, List<string>> specialFunctions = checkSpecialFunctions(ref expression);
+
+        // TODO: continue from here
+        return 0;
     }
 }
